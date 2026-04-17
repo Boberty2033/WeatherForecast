71f12fd7b45131f48a9054cd49627741e687078d
Alexander Dolgikh <alexanderdolgikh@MacBook-Pro-Alexander.local>
2026-04-17 15:27:00 +0300

    Update Model

--- WeatherForecastIOSApp/Models/WeatherModel.swift
+++ WeatherForecastIOSApp/Models/WeatherModel.swift
@@ -7,18 +7,54 @@
 
 import Foundation
 
-struct WeatherModel: Codable {
-    let name: String
-    let main: MainWeather
-    let weather: [WeatherDescription]
+struct ForecastData: Codable {
+    let list: [WeatherList]
+    let city: City
 }
 
-struct MainWeather: Codable {
+struct WeatherList: Codable {
+    let dt: Int
+    let main: MainClass
+    let weather: [WeatherInfo]
+    let wind: Wind
+    let dt_txt: String?
+}
+
+struct MainClass: Codable {
     let temp: Double
+    let feels_like: Double
+    let temp_min: Double
+    let temp_max: Double
+    let pressure: Int
     let humidity: Int
 }
 
-struct WeatherDescription: Codable {
+struct WeatherInfo: Codable {
     let description: String
     let icon: String
+    let id: Int 
+}
+
+struct City: Codable {
+    let name: String
+    let coord: Coord
+}
+
+struct Coord: Codable {
+    let lat: Double;
+    let lon: Double
+}
+
+struct SearchCityModel: Codable {
+    let name: String
+    let lat: Double
+    let lon: Double
+    let country: String
+    let state: String?
+}
+
+
+
+struct Wind: Codable {
+    let speed: Double
 }
